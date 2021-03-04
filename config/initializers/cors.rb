8d0e2b46e0a7d12bb21abe172ee0721077cc3bf4
moyongkexing <moyongkexing@gmail.com>
2021-03-04 10:19:39 +0900

    corsを設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,24 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+      origins 'http://localhost:8000'
+
+      resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+  end
+
+  #本番環境用のオリジン設定
+  allow do
+      origins 'https:<自身が設定するアプリのURL>'
+
+      resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+  end
+end
