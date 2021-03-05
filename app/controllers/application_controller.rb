8ed9c7682a85dfee709bf8cc20c22767f4c41b85
moyongkexing <moyongkexing@gmail.com>
2021-03-05 10:23:40 +0900

    認証系のコントローラーを作成を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  skip_before_action :vertify_authenticity_token
+
+  helper_method :login!, :current_user
+
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  def current_user
+    @current_uesr ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
